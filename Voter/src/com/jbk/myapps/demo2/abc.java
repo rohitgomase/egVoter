262cb9c66ecbf11202c1bbb94ad3210ec4f49cbc
Rohit Gomase <rohitgomase@gmail.com>
2022-07-11 13:36:30 +0530

    adding abc class

---
+++ Voter/src/com/jbk/myapps/demo2/abc.java
@@ -0,0 +1,13 @@
+package com.jbk.myapps.demo2;
+
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("gjgjhuk");
+	}
+
+}
